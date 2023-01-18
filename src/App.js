0d054c27fa2972ee03dcc5d3a9f9a964b39e2e8e
AssafAinT <assafain@gmail.com>
2023-01-18 09:17:59 +0200

    feed

--- src/App.js
+++ src/App.js
@@ -21,30 +21,4 @@ function App() {
     </>
   );
 }
-//   const body = 'this is a blog post';
-//   const comments = [
-//     { id: 1, text: 'comment one ' },
-//     { id: 2, text: 'comment two ' },
-//     { id: 3, text: 'comment three ' },
-//   ];
-
-//   const loading = false;
-//   const showComments = true;
-
-//   const commentBlock = (
-//     <div className="comments">
-//       <h3>Comments ({comments.length})</h3>
-//       <ul>
-//         {comments.map((comment, index) => (
-//           <li key={index}>{comment.text}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-//   if (loading) return <h1>Loading...</h1>;
-//   return React.createElement(
-//     'div',
-//     { className: 'container' },
-//     React.createElement('h1', {}, 'My App')
-//   );
 export default App;
